8eeb1d5e0d937ea0b9c749413fe3167e3af8e287
pdefusco <pauldefusco@gmail.com>
2024-05-29 13:01:41 -0700

    replaced project owner with project user env var

--- 00_datagen.py
+++ 00_datagen.py
@@ -172,7 +172,7 @@ class IotDataGen:
 
 def main():
 
-    USERNAME = os.environ["PROJECT_OWNER"]
+    USERNAME = os.environ["PROJECT_USER"]
     DBNAME = "LOGISTICS_MLOPS_{}".format(USERNAME)
     CONNECTION_NAME = "go01-aw-dl"
 
